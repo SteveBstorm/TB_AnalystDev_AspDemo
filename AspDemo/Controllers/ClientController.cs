6e0123445d8c31ba6cddd82de1470fa067440484
steveBstorm <steve.lorent@bstorm.be>
2022-03-14 17:11:08 +0100

    Début MVC

--- AspDemo/Controllers/ClientController.cs
+++ AspDemo/Controllers/ClientController.cs
@@ -1,13 +1,29 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AspDemo.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace AspDemo.Controllers
 {
     public class ClientController : Controller
     {
+
         public IActionResult Index()
         {
-            return View();
+            ViewBag.Toto = 42;
+            ViewData["truc"] = "Salut les filles";
+            TempData["maValeur"] = "super valeur qui sera affichée dans Detail";
+            return View(FakeDb.liste);
         }
 
+        public IActionResult Detail(int id)
+        {
+            
+            return View(FakeDb.GetById(id));
+        }
+
+        public IActionResult Delete(int id)
+        {
+            FakeDb.Delete(id);
+            return RedirectToAction("Index");
+        }
     }
 }
---
+++ AspDemo/Models/FakeDb.cs
@@ -0,0 +1,24 @@
+﻿namespace AspDemo.Models
+{
+    public static class FakeDb
+    {
+        public static List<Client> liste = new List<Client>() {
+                new Client { Id = 1, Name = "Arthur", Email = "arthur@mail.com" },
+                new Client { Id = 2, Name = "Perceval", Email = "Perceval@mail.com" },
+                new Client { Id = 3, Name = "Karadoc", Email = "Karadoc@mail.com" },
+                new Client { Id = 4, Name = "Merlin", Email = "Merlin@mail.com" },
+                new Client { Id = 5, Name = "Léodagan", Email = "Léodagan@mail.com" },
+                new Client { Id = 6, Name = "Bohort", Email = "Bohort@mail.com" }
+            };
+
+        public static void Delete(int id)
+        {
+            liste.Remove(liste.FirstOrDefault(x => x.Id == id));
+        }
+
+        public static Client GetById(int id)
+        {
+            return liste.FirstOrDefault(x => x.Id == id);
+        }
+    }
+}
