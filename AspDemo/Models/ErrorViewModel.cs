12451fe295c6a46ecc1a8eca3f36cc7c508802f6
steveBstorm <steve.lorent@bstorm.be>
2022-03-14 12:18:25 +0100

    Ajoutez des fichiers projet.

---
+++ AspDemo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AspDemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
